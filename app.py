9ad9c85f8dfceaa2c504ee284cda97ae02bf7424
Eswar-tech <eswaraprasath74@gmail.com>
2025-11-18 13:31:27 +0530

    Add routes for welcome and dashboard pages
    Added new routes for welcome and dashboard pages with templates.

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,request,render_template
 import os
+import math
 app = Flask(__name__)
 basename='/iotweb'
 #In the above line we use /iotweb when we serach in the localhost with localhost:7000/iot/ it give error To 
 # Resolve this we use /iotweb/ in the above line so we can search both /iotweb and /iotweb/ 
+@app.route(basename+"/ht")
+def welcome():
+   d={
+      "name":"Eswaraprasath",
+      "he":"you are in our web",
+      "avatar":"https://gitlab.com/uploads/-/system/user/avatar/742684/avatar.png?width=800"
+      
+   }
+   return render_template('bootstrap.html',data=d)
+
+@app.route(basename+"/dashboard")
+def dashboard():
+   return render_template('dash.html')
+   
 @app.route(basename+'/hai')
 def hello_world():
    return 'welcome Eswaraprasath'
@@ -30,6 +45,11 @@ def url_building(adminname):
 @app.route(basename)
 def cpuinformarmation():
    return "<pre>"+os.popen("cat /proc/cpuinfo").read()+"</pre>"
+
+@app.route(basename+'/hello',methods=['GET','POST'])
+def welcomeworld():
+   return {"result":(math.sqrt(int(request.form['num'])))}
+
 
 #we can use <Datatype:Variable_name>
 # @app.route(basename+'/<stringname>')
